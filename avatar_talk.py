862ad95a3fe378ffcc92c9ae781afdbaa1f96047
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2024-08-05 23:25:01 -0400

    Update avatar_talk.py

--- avatar_talk.py
+++ avatar_talk.py
@@ -1,18 +1,14 @@
-# from transformers import pipeline
+from transformers import pipeline
 import scipy
 
 def main():
 
-    import torch
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Using device: {device}")
+    synthesiser = pipeline("text-to-speech", "suno/bark")
 
-    # synthesiser = pipeline("text-to-speech", "suno/bark")
-    #
-    # speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
-    #
-    # scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
+    speech = synthesiser("Hello, my dog is cooler than you!", forward_params={"do_sample": True})
+
+    scipy.io.wavfile.write("bark_out.wav", rate=speech["sampling_rate"], data=speech["audio"])
 
 if __name__ == "__main__":
     main()
